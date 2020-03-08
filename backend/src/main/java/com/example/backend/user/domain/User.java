d19030bc7f427192cd404d4890146c90bb37fa7e
dnlrbz <famuho97>
2020-03-08 12:19:55 +0100

    edited config for spring rest

--- backend/src/main/java/com/example/backend/user/domain/User.java
+++ backend/src/main/java/com/example/backend/user/domain/User.java
@@ -1,13 +1,19 @@
 package com.example.backend.user.domain;
 
 import com.example.backend.base.domain.BaseEntity;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 public class User extends BaseEntity {
 
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "name")
     private String name;
 
     private User()
