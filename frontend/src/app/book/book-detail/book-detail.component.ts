a559596e4e59c6513bd9e7a0c3b8ae08662fbc03
dnlrbz <famuho97>
2020-04-09 15:03:44 +0200

    - fixed header

--- frontend/src/app/book/book-detail/book-detail.component.ts
+++ frontend/src/app/book/book-detail/book-detail.component.ts
@@ -3,6 +3,9 @@ import {ActivatedRoute} from "@angular/router";
 import {BookService} from "../service/book.service";
 import {Book} from "../domain/book";
 import {Observable} from "rxjs";
+import {addBookToCart} from "../../store/actions/cart-actions";
+import {Store} from "@ngrx/store";
+import {RootState} from "../../store/root-state";
 
 @Component({
   selector: 'book-detail',
@@ -13,12 +16,16 @@ export class BookDetailComponent implements OnInit {
 
   book$: Observable<Book>;
 
-  constructor(private route: ActivatedRoute, public bookService: BookService) {
+  constructor(private route: ActivatedRoute, public bookService: BookService, public store: Store<RootState>) {
   }
 
   ngOnInit(): void {
     const id: number = this.route.snapshot.params.id;
     this.book$ = this.bookService.getBookById(id);
+  }
+
+  addToCart(book: Book) {
+    this.store.dispatch(addBookToCart({addedBook: book}))
   }
 
 }
